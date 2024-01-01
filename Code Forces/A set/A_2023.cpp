b0f9a5ce227641b1e0d9b89043b1e3668fc12d06
saojiprasad <saojiprasad13@gmail.com>
2024-01-01 14:27:10 +0530

    commit

--- Code Forces/A set/A_2023.cpp
+++ Code Forces/A set/A_2023.cpp
@@ -6,50 +6,28 @@ void solve(){
         int n,k;
         cin>>n>>k;
         int arr[n];
-        ll pro=1;
-        bool flag1=false;
         for(int i=0;i<n;i++){
-            int a;
-            cin>>a;
-            arr[i]=a;
-            pro*=a;
-            if(a==2023||pro==2023){
-                flag1=true;
-            }
+            cin>>arr[i];
         }
-        if(flag1){
-            cout<<"YES"<<endl;
-            for(int j=0;j<k;j++){
-                cout<<1<<" ";
-            }
-            cout<<endl;
-            return;
+        bool flag=true;
+        int num=0;
+        ll pro=1;
+       for(int i=0;i<n;i++){
+        pro*=arr[i];
+        // num=2023/arr[i];
+       }
+       if(2023%pro!=0) flag=false;
+       num=2023/pro;
+       if(flag){
+        cout<<"YES"<<endl;
+        cout<<num<<" ";
+        for(int i=1;i<k;i++){
+            cout<<1<<" ";
         }
-
-        ll num=pro;
-        int i=2;
-        bool flag=false;
-        while(num<2023){
-            num=pro;
-            num*=i;
-            if(num==2023){
-                flag=true;
-                break;
-            }
-            // cout<<num<<endl;
-            i++;
-        }
-        
-        if(flag){
-            cout<<"YES"<<endl;
-            cout<<i<<' ';
-            for(int j=1;j<k;j++){
-                cout<<1<<" ";
-            }
-            cout<<endl;
-        }else{
-            cout<<"NO"<<endl;
-        }
+        cout<<endl;
+       }else{
+        cout<<"NO"<<endl;
+       }
         
 }
     
